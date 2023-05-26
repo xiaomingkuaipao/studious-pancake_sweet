5d9a55866bb2750a0509a0444640521e344ae23e
guojinfeng <g17305693276@163.com>
2023-05-26 20:06:20 +0800

    first commit

---
+++ widget.cpp
@@ -0,0 +1,312 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#include <QPixmap>
+
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+
+
+    timer=new QTimer();
+    //定时器有一个超时信号，timeout()信号
+    connect(timer,SIGNAL(timeout()),this,SLOT(timeout()));
+
+
+    //初始化蛇身
+    QRectF rect(300,250,nodeWidth,nodeHeight);
+    snake.append(rect);
+    addTop();
+    addTop();
+
+    //初始化奖品
+    addNewReword();
+
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::addTop()
+{
+    QPointF leftTop;
+    QPointF rightBotom;
+
+    if(snake[0].y()-nodeHeight<0)
+    {
+        leftTop=QPointF(snake[0].x(),this->height()-nodeHeight);
+        rightBotom=QPointF(snake[0].x()+nodeWidth,this->height());
+    }
+    else{
+
+    leftTop=QPointF(snake[0].x(),snake[0].y()-nodeHeight);
+    rightBotom=snake[0].topRight();
+
+    }
+
+    snake.insert(0,QRectF(leftTop,rightBotom));
+
+}
+
+void Widget::addDown()
+{
+    QPointF leftTop;
+    QPointF rightBotom;
+
+    if(snake[0].y()+nodeHeight*2>this->height())
+    {
+        leftTop=QPointF(snake[0].x(),0);
+        rightBotom=QPointF(snake[0].x()+nodeWidth,nodeHeight);
+    }
+    else{
+
+    leftTop=snake[0].bottomLeft();
+    rightBotom=snake[0].bottomRight()+QPointF(0,nodeHeight);
+
+    }
+
+    snake.insert(0,QRectF(leftTop,rightBotom));
+
+
+
+
+
+}
+
+void Widget::addLeft()
+{
+    QPointF leftTop;
+    QPointF rightBotom;
+
+    if(snake[0].x()-nodeWidth<0)
+    {
+        leftTop=QPointF(this->width()-nodeWidth,snake[0].y());
+
+    }
+    else{
+
+    leftTop=snake[0].topLeft()-QPointF(nodeWidth,0);
+
+
+
+    }
+    rightBotom=leftTop+QPointF(nodeWidth,nodeHeight);
+
+    snake.insert(0,QRectF(leftTop,rightBotom));
+
+}
+
+void Widget::addRight()
+{
+    QPointF leftTop;
+    QPointF rightBotom;
+
+    if(snake[0].x()+nodeWidth*2>this->width())
+    {
+        leftTop=QPointF(0,snake[0].y());
+
+    }
+    else{
+
+    leftTop=snake[0].topRight();
+
+
+
+    }
+    rightBotom=leftTop+QPointF(nodeWidth,nodeHeight);
+
+    snake.insert(0,QRectF(leftTop,rightBotom));
+
+}
+
+void Widget::deleteLast()
+{
+    snake.removeLast();
+
+}
+
+void Widget::addNewReword()
+{
+    rewardNode=QRect(qrand()%(this->width()/20)*20,qrand()%(this->height()/20)*20,nodeWidth,nodeHeight);
+
+}
+
+void Widget::collide()
+{
+//    if(snake[0].topLeft()==rewardNode)
+    //    addTop();
+}
+
+bool Widget::checkContact()
+{
+    for(int i=0;i<snake.length();i++){
+
+    for(int j=i+1;j<snake.length();j++)
+    {
+        if(snake[i]==snake[j])
+        {
+            return true;
+        }
+    }
+
+    }
+    return false;
+
+}
+
+void Widget::paintEvent(QPaintEvent *event)
+{
+    //背景图片
+
+
+        QPainter painter(this);
+
+        QPixmap pix;
+
+        painter.drawPixmap(rect(),QPixmap(":/image/image/snake.jpg"));
+
+        //画蛇
+        QPen pen;
+        pen.setColor(Qt::black);
+        QBrush brush;
+        brush.setColor(Qt::darkMagenta);
+        brush.setStyle(Qt::SolidPattern);
+
+        painter.setPen(pen);
+        painter.setBrush(brush);
+
+
+
+        for(int i=0;i<snake.length();i++)
+        {
+            painter.drawRect(snake[i]);
+        }
+
+        //画奖品
+
+        pen.setColor(Qt::red);
+
+        brush.setColor(Qt::red);
+        brush.setStyle(Qt::SolidPattern);
+
+        painter.setPen(pen);
+        painter.setBrush(brush);
+        painter.drawEllipse(rewardNode);
+
+
+        if(checkContact())
+        {
+            QFont font("方正舒体",30,QFont::ExtraLight,false);
+            painter.setFont(font);
+            painter.drawText((this->width()-300)/2,(this->height()-30)/2,QString("GAME OVER"));
+
+            timer->stop();
+
+
+        }
+
+}
+
+
+void Widget::timeout()
+{
+    int count=1;
+    //判断有没有相交或重合
+    if(snake[0].intersects(rewardNode))//相交
+    {
+        count++;
+        addNewReword();
+
+
+    }
+
+    //while(0)就是一次都不执行循环，while(1)就是一直执行循环
+    while(count--)
+    {
+    switch (moveFlag) {
+    case DIR_UP:
+       //在顶部加一个小方块
+        addTop();
+        break;
+    case DIR_DOWN:
+        addDown();
+        break;
+    case DIR_LEFT:
+        addLeft();
+
+        break;
+    case DIR_RIGHT:
+        addRight();
+
+        break;
+
+
+    default:
+        break;
+    }
+}
+    deleteLast();
+    update();
+}
+
+//控制方向
+void Widget::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_Up:
+        if(moveFlag!=DIR_DOWN)
+        {
+           moveFlag=DIR_UP;
+
+        }
+
+        break;
+    case Qt::Key_Down:
+        if(moveFlag!=DIR_UP)
+        {
+          moveFlag=DIR_DOWN;
+        }
+        break;
+    case Qt::Key_Left:
+        if(moveFlag!=DIR_RIGHT)
+        {
+          moveFlag=DIR_LEFT;
+        }
+        break;
+    case Qt::Key_Right:
+        if(moveFlag!=DIR_LEFT)
+        {
+          moveFlag=DIR_RIGHT;
+        }
+        break;
+    case Qt::Key_Space:
+        if(gameStart==false)
+        {
+
+           gameStart=true;
+           //启动定时器
+           timer->start(100);
+        }
+        else{
+
+            gameStart=false;
+
+            timer->stop();
+        }
+
+
+        break;
+
+    default:
+        break;
+    }
+}
+
---
+++ widget.h
@@ -0,0 +1,103 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+#include <QKeyEvent>
+#include <QTimer>
+#include <QList>
+#include <QPointF>
+#include <QRectF>
+#include <QPainter>
+
+//1.按键处理
+//2.使用定时器
+// 2.1 关联信号槽 2.2启动定时器 2.3实现对应的超时处理函数
+//3.渲染
+//4.蛇的表示，长度可变的数组
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+
+//定义一个变量表示方向,定义在里面或外面都行
+enum Director{
+
+    DIR_LEFT,
+    DIR_RIGHT,
+    DIR_UP,
+    DIR_DOWN
+
+};
+
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    int moveFlag=DIR_UP;
+    bool gameStart=false;
+
+    //定时器
+    QTimer *timer;
+
+    int time=100;//超时时间间隔
+
+    void addTop();
+    void addDown();
+    void addLeft();
+    void addRight();
+
+    void deleteLast();//删除最后一个
+
+    //添加奖品
+    void addNewReword();
+
+    //
+    void collide();
+
+    //蛇头碰蛇身
+    bool checkContact();
+
+
+    //蛇，由小方块组成
+    QList <QRectF> snake;
+    //奖品
+    QRectF rewardNode;
+
+    //小方块的大小
+    int nodeWidth=20;
+    int nodeHeight=20;
+
+    //绘画
+    //void paintEvent(QPainter *event);
+
+protected:
+    void    paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+
+
+
+public slots:
+     void timeout();
+
+
+
+
+
+
+//按键事件
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
